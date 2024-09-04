72501857d8d9ac084e02d1e0e05ab87181898cb8
saad1551 <saad.ashraf.9094@gmail.com>
2024-09-04 19:03:56 -0400

    task1 done

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,18 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
+
+    const handleAddToCart = (plant) => {
+        dispatch(addItem(plant));
+        const newAddedToCart = addedToCart;
+        newAddedToCart[plant.name] = true;
+        setAddedToCart(newAddedToCart);
+    };
 
     const plantsArray = [
         {
