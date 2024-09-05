d78ae03491eb46eac7204143dd89e308bfabce2d
saad1551 <saad.ashraf.9094@gmail.com>
2024-09-05 10:20:02 -0400

    change add-to-cart btn to grey after product added to cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -291,7 +291,7 @@ const handlePlantsClick = (e) => {
                                 <div className="product-title">{plant.name}</div>
                                 <div className="product-description">{plant.description}</div>
                                 <div className="product-price">{plant.cost}</div>
-                                <button className="product-button" disabled={addedToCart[plant.name]} onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                <button className={`product-button ${addedToCart[plant.name] ? 'added-to-cart' : ''}`} disabled={addedToCart[plant.name]} onClick={() => handleAddToCart(plant)}>{!addedToCart[plant.name] ? 'Add To Cart' : 'Added To Cart'}</button>
                             </div>
                         ))}
                     </div>
